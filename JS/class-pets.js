7a46ba07cbcaf593b801908747e9cbe2dc94587f
Marcus Vinicius <marcusdevinicius027@gmail.com>
2025-04-07 17:15:05 -0300

    Finalizando
    Restando apenas alguns ajustes mínimos e alguns códigos de testes para apagar.

--- JS/class-pets.js
+++ JS/class-pets.js
@@ -1,5 +1,5 @@
 export default class PetsIdade {
-    constructor(pet, petIdade, petPeso, form, btn, result, containerPeso) {
+    constructor(pet, petIdade, petPeso, form, btn, result, containerPeso, erroPet, erroIdd, erroPeso) {
         this.pet = pet;
         this.petIdade = petIdade;
         this.petPeso = petPeso;
@@ -7,8 +7,12 @@ export default class PetsIdade {
         this.btn = btn;
         this.result = result;
         this.containerPeso = containerPeso;
-        this.arrayFelino = ['GATO', 'GATA'];
-        this.arrayCannis = ['CÃO', 'CAO', 'CACHORRO', 'CACHORRA', 'CADELA'];
+        this.arrayFelino = ['gato', 'gata'];
+        this.arrayCannis = ['cão', 'cao', 'cachorro', 'cachorra', 'cadela'];
+        this.petsCadastrados = this.arrayCannis.concat(this.arrayFelino);
+        this.erroPet = erroPet;
+        this.erroIdd = erroIdd;
+        this.erroPeso = erroPeso;
 
         //binds
         this.handleChange = this.handleChange.bind(this);
@@ -22,7 +26,7 @@ export default class PetsIdade {
     }
 
     handleChange() {
-        const petValue = this.pet.value.toUpperCase();
+        const petValue = this.pet.value.toLowerCase();
         const idadeValue = this.petIdade.value;
         const pesoValue = this.petPeso.value;
         if(this.arrayFelino.includes(petValue)) {
@@ -32,22 +36,27 @@ export default class PetsIdade {
             this.containerPeso.classList.remove('inativo');
 
         }
+        this.verificarInfo(petValue, idadeValue, pesoValue);
     }
 
     handleClick() {
-        const petValue = this.pet.value.toUpperCase().trim();
+        const petValue = this.pet.value.toLowerCase().trim();
         const idadeValue = this.petIdade.value;
         const pesoValue = this.petPeso.value;
         // console.log(petValue, idadeValue, pesoValue)
         const iddValueExtenso = this.numParaExtenso(idadeValue);
+
         this.buscarIdade(petValue, iddValueExtenso, pesoValue);
+        this.verificarInfo(petValue, idadeValue, pesoValue);
     }
 
     buscarIdade(petValue, iddValueExtenso, pesoValue) {
         console.log(petValue, iddValueExtenso, pesoValue);
         if(this.arrayCannis.includes(petValue)) {
             this.dadosDog(petValue, iddValueExtenso, pesoValue);
-            console.log('the chosen one was a dog')
+            console.log('the chosen one was a dog');
+        } else if(this.arrayFelino.includes(petValue)) {
+            this.dadosCat(petValue, iddValueExtenso)
         }
     }
 
@@ -63,11 +72,70 @@ export default class PetsIdade {
                     iddDogHuman = jsonContent.dezmenos[iddValueExtenso];
                 } else if(pesoValue >= 10 && pesoValue < 22) {
                     iddDogHuman = jsonContent.dezevintedois[iddValueExtenso];
+                }  else if(numero >= 22) {
+                    iddDogHuman = jsonContent.vintedoismais[iddValueExtenso];
                 }
-                this.result.innerText = `Seu pet possui em média ${iddDogHuman} anos em relação aos humanos.`;
+                // fazer tratamento de string e retornar nome do 
+                // pet com 1º letra maiuscula
+                const petValueTratado = petValue.charAt(0).toUpperCase() + petValue.slice(1);
+                console.log(petValueTratado);
+                if (!this.result.classList.contains('ativo')) {
+                    this.result.classList.add('ativo');
+                }
+                
+                // verificando se é masculino ou feminino, para mudar o pronome
+                if(petValue === 'cão' || petValue === 'cao' || petValue === 'cachorro') {
+                    this.result.innerText = `Seu ${petValueTratado} possui em média ${iddDogHuman} anos em relação aos humanos.`;
+                } else if(petValue === 'cadela' || petValue === 'cachorra') {
+                    this.result.innerText = `Sua ${petValueTratado} possui em média ${iddDogHuman} anos em relação aos humanos.`;
+                }
             })
         });
     }
+
+    dadosCat(petValue, iddValueExtenso) {
+        // fazer tratamento de string e retornar nome do 
+        // pet com 1º letra maiuscula
+        const petValueTratado = petValue.charAt(0).toUpperCase() + petValue.slice(1);
+        console.log(petValueTratado);
+
+        fetch('JS/dados-pets.json')
+        .then(response => {
+            const jsonPets = response.json();
+            jsonPets.then(jsonContent => {
+                const iddCatHuman = jsonContent.gatos[iddValueExtenso];
+                if (!this.result.classList.contains('ativo')) {
+                    this.result.classList.add('ativo');
+                }
+
+                // verificando se é masculino ou feminino, para mudar o pronome
+                if (petValue === 'gato') {
+                    this.result.innerText = `Seu ${petValueTratado} tem em média ${iddCatHuman} anos em idade de humanos. `
+                } else if(petValue === 'gata') {
+                    this.result.innerText = `Sua ${petValueTratado} tem em média ${iddCatHuman} anos em idade de humanos. `
+                }
+            })
+        })
+    }
+
+    // Verifica se o que o usuário colocou no forms está cadastrado
+    // como: ver se há informações para determinado animal ou se 
+    // o usuário colocou as informações idade e peso dentro dos limites
+    verificarInfo(petValue, idadeValue, pesoValue) {
+        if(!this.petsCadastrados.includes(petValue)) {
+            this.erroPet.innerText = 'Ainda não temos dados sobre essa espécie, tente novamente com outro(Cão, gato...).';
+            this.result.innerText = '';    
+        } if(this.petsCadastrados.includes(petValue)) {
+            this.erroPet.innerText = '';
+        } 
+        if(idadeValue < 1 || idadeValue > 15) {
+            this.erroIdd.innerText = 'Os estudos sobre pets demonstram com idades de 1 a 15 anos.'
+            console.log(idadeValue);
+        } if(idadeValue > 1 && idadeValue < 16) {
+            this.erroIdd.innerText = '';
+        }
+    }
+
 
     // Transforma o numeral em extenso
     // nesse caso o valor da idade 
@@ -79,10 +147,11 @@ export default class PetsIdade {
             return unidades[numero];
         }else if(numero < 20) {
             return menorQueVinte[numero - 10];
-        }
-        
+        }   
     }
 
+    // Método inicial, de onde todos os outros sairam, pois 
+    // utilizei uma forma de função chamando função durante a classe
     init() {
         this.addEventListeners();
     }
--- JS/sript.js
+++ JS/sript.js
@@ -6,7 +6,10 @@ const pesoInput = document.querySelector('.peso-pet');
 const form = document.querySelector('[data-form="form"]');
 const btnCalc = document.querySelector('[data-form="btn-calc"]');
 const resultFinal = document.querySelector('[data-form="result"]');
-const containerPeso = document.querySelector('.container-peso')
+const containerPeso = document.querySelector('.container-peso');
+const erroPet = document.querySelector('.erro-pet');
+const erroIdd = document.querySelector('.erro-idd');
+const erroPeso = document.querySelector('.erro-peso');
 
-const pet = new PetsIdade(petInput, idadeInput, pesoInput, form, btnCalc, resultFinal, containerPeso);
+const pet = new PetsIdade(petInput, idadeInput, pesoInput, form, btnCalc, resultFinal, containerPeso, erroPet, erroIdd, erroPeso);
 pet.init();
